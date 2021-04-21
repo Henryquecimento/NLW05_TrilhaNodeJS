938919351f4e796559adf3536f999a823425b586
Henryque <henryque_1@hotmail.com>
2021-04-21 16:18:59 -0300

    Users and Messages controllers and its services, entities and migrations  added -- 21/04/21

---
+++ src/services/UsersService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    //check if user already exists
+
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
+    //if not exists, save in the DB
+    if (userExists) {
+      return userExists;
+    }
+
+    //If exists, return user
+    const user = this.usersRepository.create({
+      email,
+    });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
